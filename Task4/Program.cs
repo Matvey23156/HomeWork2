3876d2d5eac8c600418d1fa60bfb650f892d7264
Matvey Kolomov <matvey.kolomov134@gmail.com>
2024-02-05 16:20:54 +1000

    Task4:HonewWork2

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.Write("Введите любое натуральное число N: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[n];
+
+int i = 0;
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+Console.Write($"{arr[i]} ");
+i = i + 1;
+}
