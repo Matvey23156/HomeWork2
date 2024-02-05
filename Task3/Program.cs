0ba8498b0462757757d02f7092cc0d1289491cc3
Matvey Kolomov <matvey.kolomov134@gmail.com>
2024-02-05 16:18:50 +1000

    Task3:HomeWork2

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает
+// на вход целое число из отрезка [10,99] 
+// и показывает наибольшую цифру числа
+
+
+Console.Write("Введите число из отрезка [10, 99] и нажмите клавишу Enter: "); 
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstNumber = number / 10;
+int secondNumber = number % 10;
+int max = 0;
+
+if (firstNumber > secondNumber)
+{
+    max = firstNumber;
+}
+else
+{
+    max = secondNumber;
+}
+Console.WriteLine(max);
